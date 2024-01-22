62865d79ae89d44506327d87f97fd2c31ad6ae76
Cycrypto <jh01love00@gmail.com>
2024-01-22 17:14:39 +0900

    add: add 함수 추가

--- week1/api_dev/utility/frozen_json.py
+++ week1/api_dev/utility/frozen_json.py
@@ -1,6 +1,5 @@
 from collections import abc
 
-
 class FacadeJSON:
     def __init__(self, mapping):
         self.__data = dict(mapping)
@@ -19,3 +18,23 @@ class FacadeJSON:
             return [cls.build(item) for item in obj]
         else:
             return obj
+
+    def to_json(self):
+        if isinstance(self.__data, abc.Mapping):
+            return {key: value.to_json()
+            if isinstance(value, FacadeJSON) else value for key, value in self.__data.items()}
+        elif isinstance(self.__data, abc.MutableSequence):
+            return [item.to_json() if isinstance(item, FacadeJSON) else item for item in self.__data]
+        else:
+            return self.__data
+
+    def add(self, new_block):
+        if isinstance(self.__data, dict) and 'template' in self.__data:
+            template = self.__data['template']
+            if isinstance(template, dict) and 'outputs' in template:
+                outputs = template['outputs']
+                if isinstance(outputs, list):
+                    outputs.append(new_block)
+                    return
+
+        raise ValueError("Invalid structure for adding output")
