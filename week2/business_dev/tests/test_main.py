9273c7c5db9413d0444960b371a99a6ff27f3a26
Cycrypto <jh01love00@gmail.com>
2024-01-27 11:17:22 +0900

    Apply .gitignore

---
+++ week2/business_dev/tests/test_main.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from week2.business_dev.main import run
+
+
+class TestCrawlingInfo(TestCase):
+    def test_create_result(self):
+        result = run()
+        self.assertIn('informations', result.keys())
+        for k in result['informations']:
+            for v in ['title', 'author', 'date', 'link']:
+                self.assertIn(v, k)
