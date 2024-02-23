9ea53799c1cf5b0419f7f7ce16239d6d0feb189a
Cycrypto <jh01love00@gmail.com>
2024-02-23 14:02:39 +0900

    Apply .gitignore

--- week1/business_dev/main.py
+++ week1/business_dev/main.py
@@ -1,5 +1,14 @@
+import json
+import requests
+import pprint
+
+
 def app_main(data: dict) -> dict:
-    pass
+    url = r"http://swopenapi.seoul.go.kr/api/subway/sample/json/realtimeStationArrival/0/5/%EC%A0%95%EC%99%95"
+    req = requests.get(url)
+    data = json.loads(req.text)
+    print(type(req.text))
+    pprint.pprint(data['realtimeArrivalList'][0]['arvlMsg3'])
 
 
 if __name__ == "__main__":
